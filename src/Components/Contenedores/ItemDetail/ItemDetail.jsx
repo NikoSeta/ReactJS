618e7f056717072337d5814091b349cd7b8e72fb
Niko De Seta <desetanico@gmail.com>
2021-10-17 16:52:40 -0300

    detail solucionado

--- src/Components/Contenedores/ItemDetail/ItemDetail.jsx
+++ src/Components/Contenedores/ItemDetail/ItemDetail.jsx
@@ -3,24 +3,28 @@ import {useAppContext} from '../../../Context/AppContext'
 
 function ItemDetail({auto})  { 
     const { agregarAlCarrito } = useAppContext()
-    const {imgUrl, marca, modelo, año, descripcion, precio} = auto
+    const {imgId, marca, modelo, año, descripcion, precio} = auto
     const addItem=(cant)=>{  
         console.log(cant);
         agregarAlCarrito(auto, cant)        
     }
     return (
         <>
-            <div className="col-md-4">
-                <img src={imgUrl} className="img-fluid rounded-start" alt="..."/>
-            </div>
-            <div className="col-md-8">
-                <div className="card-body">
-                    <h5 className="card-title">{marca} {modelo} {año}</h5>
-                    <p className="card-text">{descripcion}</p>
-                    <h5 className="card-text">${precio}</h5>   
-                    <ItemCount initial={1} addItem={addItem}/>
+        <div style={{maxwidth: "1000px"}}>
+            <div className="row g-0">
+                <div className="col-md-4">
+                    <img src={imgId} className="img-fluid rounded-start" alt={marca}/>
+                </div>
+                <div className="col-md-8">
+                    <div className="card-body">
+                        <h5 className="card-title">{marca} {modelo} {año}</h5>
+                        <p className="card-text">{descripcion}</p>
+                        <h5 className="card-text">${precio}</h5> 
+                        <ItemCount initial={1} addItem={addItem}/> 
                     </div>
                 </div>
+            </div>
+        </div>
         </>
     )
 }
